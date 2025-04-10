cf379ce5e0ba9f051aedf28d303a8164a3d0a71b
clementAvesque <clement2avesque@gmail.com>
2025-04-10 11:32:44 +0200

    push des correction du html de base

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Article from '../views/Article.vue'
+import Home from '../views/HomePage.vue'
 
 const routes = [
   {
     path: '/article', // URL que tu veux, par ex. /article
     name: 'Article',
     component: Article
+  },
+  {
+    path: '/', 
+    name: 'HomePage',
+    component: Home
   }
 ]
 
---
+++ src/views/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+    <section id="page">
+        <section id="pres">
+            <div id="back">
+                <img src="../images/background_pres.png" alt="">
+            </div>
+
+            <p>
+                t'es pas beau
+            </p>
+        </section>
+    </section>
+</template>
+<script>
+</script>
+
+<style scoped>
+#pres{
+    background-image: url("../images/background_pres.png");
+    width: 100vw;
+    height: auto;
+}
+
+p{
+    margin: 0%;
+}
+
+#back img{
+    width: 100%;
+    height: auto;
+    position: absolute;
+}
+</style>
